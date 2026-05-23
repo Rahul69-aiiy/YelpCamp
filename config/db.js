729f7ccd62f24378e6f1ce0d7154b568a3c4a008
Rahul69-aiiy <prataprahul68@gmail.com>
2026-05-23 11:45:40 +0530

    fix

--- config/db.js
+++ config/db.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose')
 
 const connectDB = async (dbUrl) => {
-    mongoose.connect(dbUrl);
-    const db = mongoose.connection
-    db.on("error", console.error.bind(console, "connection error:"))
-    db.once("open", () => {
-        console.log("Database connected")
-    })
+    try {
+        const db = mongoose.connection;
+        db.on("error", console.error.bind(console, "connection error:"));
+        db.once("open", () => {
+            console.log("Database connected");
+        });
+        await mongoose.connect(dbUrl);
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 module.exports = connectDB;
